4b20431bfb3d315225aec0e3d33beb13a6fa257e
sw-develop <anikapsw@gmail.com>
2020-10-01 18:22:30 +0900

    장고 관리자에 모델 등록

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Customer, Product, Order
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+
+
 # Register your models here.
