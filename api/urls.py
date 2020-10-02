ac21d44820ca2a7706958105c4b391d8dd303983
sw-develop <anikapsw@gmail.com>
2020-10-02 20:40:48 +0900

    Product, Choice 추가

--- api/urls.py
+++ api/urls.py
@@ -6,8 +6,12 @@ from . import views
 urlpatterns = [
     path('users/', views.UserListAPIView.as_view()),
     path('users/<int:pk>/', views.UserDetailAPIView.as_view()),
-
-
+    path('customers/', views.CustomerListAPIView.as_view()),
+    path('customers/<int:pk>/', views.CustomerDetailAPIView.as_view()),
+    path('products/', views.ProductListAPIView.as_view()),
+    path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('choices/', views.ChoiceListAPIView.as_view()),
+    path('choices/<int:pk>/', views.ChoiceDetailAPIView.as_view()),
 
 ]
 
