957fa2255f87fd22eb141ce128d6917619985934
sw-develop <anikapsw@gmail.com>
2020-10-02 15:53:44 +0900

    admin 등록, 보여질 필드 목록 설정

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Customer, Product, Order
+from .models import Customer, Product, Choice
 
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['user', 'addr', 'membership']#Admin 목록에 보여질 필드 목록
 
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'product', 'quantity']
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'price', 'color', 'size']
