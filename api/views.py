0df5d6d9a26bad6a83c805af82ad3719be3c5eed
sw-develop <anikapsw@gmail.com>
2020-10-31 04:16:12 +0900

    add category & cart

--- api/views.py
+++ api/views.py
@@ -1,167 +1,47 @@
 from django.shortcuts import render
-from .serializers import UserSerializer, CustomerSerializer, ProductSerializer, ChoiceSerializer
+from .serializers import UserSerializer, CustomerSerializer, ProductSerializer, ChoiceSerializer, CartSerializer, CategorySerializer
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import authentication, permissions, status
 from django.contrib.auth.models import User
-from .models import Customer, Product, Choice
+from .models import Customer, Product, Choice, Cart, Category
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
 
 #post -> 생성, get -> 조회, put -> 수정, delete->삭제
 #ListAPIView:모델 객체 목록 조회 및 새로운 객체 생성(get, post) -> 목록에 대한
 #DetailAPIView:객체 내용, 수정, 삭제(get, put, delete) -> 특정 객체에 대한 
 
+#ModelViewSet: list, create, retrieve, update, destroy actions 제공
+
 #User
-class UserListAPIView(APIView):
-    def get(self, request, format=None):#urls.py에서 format_suffix_patterns 사용하기 위해 format=None 쓰임
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)#POST통한 리소스 생성 작업 성공시
-        return Response(serializer.errors, status=400)#클라이언트 요청 부적절 시 사용
-
-from django.shortcuts import get_object_or_404
-
-class UserDetailAPIView(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(User, pk=pk)
-    """
-    밑의 코드랑 같은 의미
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExit:
-            raise Http404
-    """
-
-    def get(self, request, pk, format=None):#format옵션 뭐지?
-        user = self.get_object(pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        user = self.get_object(pk)
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        user = self.get_object(pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 
 #Customer
-class CustomerListAPIView(APIView):
-    def get(self, request, format=None):
-        customers = Customer.objects.all()
-        serializer = CustomerSerializer(customers, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = CustomerSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-
-class CustomerDetailAPIView(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(Customer, pk=pk)
-
-    def get(self, request, pk, format=None):
-        customer = self.get_object(pk)
-        serializer = CustomerSerializer(customer)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        customer = self.get_object(pk)
-        serializer = CustomerSerializer(customer, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        user = self.get_object(pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class CustomerViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomerSerializer
+    queryset = Customer.objects.all()
 
 #product
-class ProductListAPIView(APIView):
-    def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
 
-    def post(self, request, format=None):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)#POST통한 리소스 생성 작업 성공시
-        return Response(serializer.errors, status=400)#클라이언트 요청 부적절 시 사용
-
-
-class ProductDetailAPIView(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(Product, pk=pk)
-
-    def get(self, request, pk, format=None):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        product = self.get_object(pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#Category
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
 
 #choice
-class ChoiceListAPIView(APIView):
-    def get(self, request, format=None):
-        choices = Choice.objects.all()
-        serializer = ChoiceSerializer(choices, many=True)
-        return Response(serializer.data)
+class ChoiceViewSet(viewsets.ModelViewSet):
+    serializer_class = ChoiceSerializer
+    queryset = Choice.objects.all()
 
-    def post(self, request, format=None):
-        serializer = ChoiceSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-class ChoiceDetailAPIView(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(Choice, pk=pk)
-
-    def get(self, request, pk, format=None):
-        choice = self.get_object(pk)
-        serializer = ChoiceSerializer(choice)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        choice = self.get_object(pk)
-        serializer = ChoiceSerializer(choice, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        choice = self.get_object(pk)
-        choice.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#Cart
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerializer
+    queryset = Cart.objects.all()
