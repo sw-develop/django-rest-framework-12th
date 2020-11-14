3368c34f8f5538b686d2925b1a80e75011c80aee
sw-develop <anikapsw@gmail.com>
2020-11-15 00:28:20 +0900

    authentication_classes 추가

--- api_server/settings.py
+++ api_server/settings.py
@@ -71,6 +71,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ]
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 """
 
