e5b29fc5a077fb94788e53a3d049fd05e1801ea8
sw-develop <anikapsw@gmail.com>
2020-11-28 12:16:16 +0900

    permission denied message

--- api/permissions.py
+++ api/permissions.py
@@ -1,9 +1,8 @@
 from rest_framework import permissions
 
-
 #상품&카테고리 등록 및 삭제는 스태프에게만 허용
 class IsStaffOrReadonly(permissions.BasePermission):
-    message = 'Adding, Deleting not allowed'
+    #message = 'Adding, Deleting not allowed' -> 인증 부분 구현하지 않아서 확인불가 -> 인증 통과한 후, permission denied 실행
 
     def has_permission(self, request, view): #로그인에 상관없이 모두에게
         return True
