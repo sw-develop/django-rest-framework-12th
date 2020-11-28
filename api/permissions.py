cd1034f2a11237892e7fa433485752cdeb0c0b38
sw-develop <anikapsw@gmail.com>
2020-11-28 11:48:50 +0900

    custom permission 추가

--- api/permissions.py
+++ api/permissions.py
@@ -1,15 +1,11 @@
 from rest_framework import permissions
-from rest_framework.permissions import BasePermission
-
-#카트는 로그인한 유저에게만 보여지도록
-class IsAuthenticated(BasePermission):
-    def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated)
 
 
 #상품&카테고리 등록 및 삭제는 스태프에게만 허용
-class AllowAny(BasePermission):
-    def has_permission(self, request, view):
+class IsStaffOrReadonly(permissions.BasePermission):
+    message = 'Adding, Deleting not allowed'
+
+    def has_permission(self, request, view): #로그인에 상관없이 모두에게
         return True
 
     def has_object_permission(self, request, view, obj):
