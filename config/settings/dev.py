3ecc782d7191c59f1128b87ffc2dc39b0c4abac9
sw-develop <anikapsw@gmail.com>
2020-12-29 21:32:18 +0900

    [ADD] db secrets

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,15 +5,4 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-
-DATABASES = {
-    "default" : {
-      "ENGINE": "django.db.backends.mysql",
-      "NAME": "postdb",
-      "USER": "admin",
-      "PASSWORD": "tpdnjs719",
-      "HOST": "localhost",
-      "PORT": "3306"
-    }
-}
+DATABASES = secrets["LOCAL_SETTINGS"]
